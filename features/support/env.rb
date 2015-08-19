48af9f31897f8f52686bf00436ef0ef7169f022d
Dave Huff <David.Huff@computer-critters.com>
2015-08-18 21:41:27 -0400

    trying to get it working

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,14 +8,14 @@ World(Gametel::Navigation)
 
 keystore = {
   # :path => File.expand_path('~/.android/debug.keystore'),
-  :path => File.dirname(__FILE__) + './debug.keystore',
+  :path => File.dirname(__FILE__) + '/debug.keystore',
   :alias => 'androiddebugkey',
   :password => 'android',
   :keystore_password => 'android'
 }
 
 # Gametel.apk_path = PATH_TO_APK
-Gametel.apk_path = File.dirname(__FILE__) + './puppy-app.apk'
+Gametel.apk_path = File.dirname(__FILE__) + '/puppy-app.apk'
 Gametel.keystore = keystore
 
 Before do
