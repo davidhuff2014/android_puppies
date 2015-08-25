9516a04085b96fe51d91fc98279a6ff25417e1ab
Dave Huff <David.Huff@computer-critters.com>
2015-08-25 11:41:14 -0400

    build out steps

--- features/step_definitions/displaying_the_details_of_a_puppy_steps.rb.rb
+++ features/step_definitions/displaying_the_details_of_a_puppy_steps.rb.rb
@@ -1,27 +1,27 @@
-When(/^I want to learn more information about "([^"]*)"$/) do |arg|
-  pending
+When(/^I want to learn more information about "([^"]*)"$/) do |name|
+  on(PuppyListScreen).details_for name
 end
 
-Then(/^I will be able to see "([^"]*)"s details$/) do |arg|
-  pending
+Then(/^I will be able to see "([^"]*)"s details$/) do |name|
+  expect(on(PuppyDetailsScreen).name).to eq name
 end
 
-Then(/^I know that he is a type of "Australian Cattle Dog \(Blue Heeler\)"$/) do |arg|
-  pending
+Then(/^I know that he is a type of "([^"]*)"$/) do |breed|
+  expect(on(PuppyDetailScreen).breed).to eq breed
 end
 
-And(/^I know that he is a "([^"]*)"$/) do |arg|
-  pending
+And(/^I know that he is a "([^"]*)"$/) do |gender|
+  expect(on(PuppyDetailScreen).gender).to eq gender
 end
 
-Then(/^I can see that my puppy "([^"]*)"$/) do |arg|
-  pending
+Then(/^I can see that my puppy "([^"]*)"$/) do |description|
+  expect(on(PuppyDetailScreen).description).to eq description
 end
 
 Then(/^I can look into the eyes of my puppy before I make my decision$/) do
-  pending
+  on(PuppyDetailScreen).wait_for_headshot
 end
 
-Then(/^I know that the adoption fee is "\$42\.00"$/) do |arg|
-  pending
+Then(/^I know that the adoption fee is "([^"]*)"$/) do |adoption_fee|
+  expect(on(PuppyDetailScreen).fee).to eq adoption_fee
 end
